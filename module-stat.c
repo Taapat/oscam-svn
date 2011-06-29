Taapat/oscam-svn

commit 2131af57f705b9d23c1d6796c011f70908c5246a

loadbalancer: now reader-found by cache does not add a "rc 0 80000" stat entry
git-svn-id: https://svn.streamboard.tv/oscam/trunk@5504 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-stat.c
@@ -447,8 +447,9 @@ void add_stat(struct s_reader *rdr, ECM_REQUEST *er, int32_t ecm_time, int32_t r
 	}
 	else if (rc == 1 || rc == 2) { //cache
 		//no increase of statistics here, cachetime is not real time
-		stat = get_add_stat(rdr, er, prid);
-		stat->last_received = ctime;
+		stat = get_stat(rdr, er->caid, prid, er->srvid, er->l);
+		if (stat != NULL)
+			stat->last_received = ctime;
 	}
 	else if (rc == 4) { //not found
 		//CCcam card can't decode, 0x28=NOK1, 0x29=NOK2

commit 2bb07ae3ac9ef72cb4d2244cf60c4030ae4707a7

Fix segfault introduced in r5504
git-svn-id: https://svn.streamboard.tv/oscam/trunk@5506 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-stat.c
@@ -450,6 +450,7 @@ void add_stat(struct s_reader *rdr, ECM_REQUEST *er, int32_t ecm_time, int32_t r
 		stat = get_stat(rdr, er->caid, prid, er->srvid, er->l);
 		if (stat != NULL)
 			stat->last_received = ctime;
+		return;
 	}
 	else if (rc == 4) { //not found
 		//CCcam card can't decode, 0x28=NOK1, 0x29=NOK2