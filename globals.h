Taapat/oscam-svn

commit 34a43453c34f0cffe6e4abb2d6ec3928f926c0d2

Add default values for CCcam reshare,maxhop and stealth
git-svn-id: https://svn.streamboard.tv/oscam/trunk@5762 4b0bc96b-bc66-0410-9d44-ebda105a78c1

globals.h
@@ -392,6 +392,17 @@ extern void cs_switch_led(int32_t led, int32_t action);
 #define LB_MAX_STAT_TIME		10
 
 /* ===========================
+ *      Default Values
+ * =========================== */
+#define DEFAULT_TCP_RECONNECT_TIMEOUT 30
+
+#ifdef MODULE_CCCAM
+#define DEFAULT_CC_MAXHOP  10
+#define DEFAULT_CC_RESHARE -1 // Use global cfg
+#define DEFAULT_CC_STEALTH -1 // Use global cfg
+#endif
+
+/* ===========================
  *      global structures
  * =========================== */
 typedef struct cs_mutexlock {
@@ -946,6 +957,7 @@ struct s_reader  									//contains device info, reader info and card info
 	uint32_t		cc_id;
 	int8_t			cc_keepalive;
 	int8_t			cc_hop;							// For non-cccam reader: hop for virtual cards
+	int8_t			cc_reshare;
 #endif
 	int8_t			tcp_connected;
 	int32_t			tcp_ito;						// inactivity timeout
@@ -1020,9 +1032,6 @@ struct s_reader  									//contains device info, reader info and card info
 	BIGNUM			ucpk;
 	////variables from reader-viaccess.c
 	struct geo_cache	last_geo;
-#ifdef MODULE_CCCAM
-	int32_t			cc_reshare;
-#endif
 #ifdef WITH_LB
 	int32_t			lb_weight;						//loadbalance weight factor, if unset, weight=100. The higher the value, the higher the usage-possibility
 	int32_t			lb_usagelevel;					//usagelevel for loadbalancer
@@ -1099,7 +1108,7 @@ struct s_auth
 	uint8_t			c35_sleepsend;
 	int8_t			ncd_keepalive;
 	int32_t			cccmaxhops;
-	int32_t			cccreshare;
+	int8_t			cccreshare;
 	int8_t			cccignorereshare;
 	int8_t			cccstealth;
 	int8_t			disabled;

oscam-config.c
@@ -1759,12 +1759,12 @@ void chk_account(const char *token, char *value, struct s_auth *account)
 
 #ifdef MODULE_CCCAM
 	if (!strcmp(token, "cccmaxhops")) {
-		account->cccmaxhops = strToIntVal(value, 10);
+		account->cccmaxhops = strToIntVal(value, DEFAULT_CC_MAXHOP);
 		return;
 	}
 
 	if (!strcmp(token, "cccreshare")) {
-		account->cccreshare = strToIntVal(value, -1);
+		account->cccreshare = strToIntVal(value, DEFAULT_CC_RESHARE);
 		return;
 	}
 
@@ -1776,7 +1776,7 @@ void chk_account(const char *token, char *value, struct s_auth *account)
 	}
 
 	if (!strcmp(token, "cccstealth")) {
-		account->cccstealth = strToIntVal(value, -1);
+		account->cccstealth = strToIntVal(value, DEFAULT_CC_STEALTH);
 		return;
 	}
 #endif
@@ -2518,16 +2518,16 @@ int32_t write_userdb()
 			fprintf_conf(f, "suppresscmd08", "%d\n", account->c35_suppresscmd08);
 
 #ifdef MODULE_CCCAM
-		if (account->cccmaxhops != 10 || cfg.http_full_cfg)
+		if (account->cccmaxhops != DEFAULT_CC_MAXHOP || cfg.http_full_cfg)
 			fprintf_conf(f, "cccmaxhops", "%d\n", account->cccmaxhops);
 
-		if ((account->cccreshare != -1) || cfg.http_full_cfg)
+		if ((account->cccreshare != DEFAULT_CC_RESHARE) || cfg.http_full_cfg)
 			fprintf_conf(f, "cccreshare", "%d\n", account->cccreshare);
 
 		if ((account->cccignorereshare != cfg.cc_ignore_reshare && account->cccignorereshare != -1) || cfg.http_full_cfg)
 			fprintf_conf(f, "cccignorereshare", "%d\n", account->cccignorereshare);
 
-		if ((account->cccstealth != -1) || cfg.http_full_cfg)
+		if ((account->cccstealth != DEFAULT_CC_STEALTH) || cfg.http_full_cfg)
 			fprintf_conf(f, "cccstealth", "%d\n", account->cccstealth);
 #endif
 
@@ -2640,7 +2640,7 @@ int32_t write_server()
 			if ((rdr->tcp_ito || cfg.http_full_cfg) && !isphysical && rdr->typ != R_CCCAM)
 				fprintf_conf(f, "inactivitytimeout", "%d\n", rdr->tcp_ito);
 
-			if ((rdr->tcp_rto != 30 || cfg.http_full_cfg) && !isphysical)
+			if ((rdr->tcp_rto != DEFAULT_TCP_RECONNECT_TIMEOUT || cfg.http_full_cfg) && !isphysical)
 				fprintf_conf(f, "reconnecttimeout", "%d\n", rdr->tcp_rto);
 
 			if ((rdr->ncd_disable_server_filt || cfg.http_full_cfg) && rdr->typ == R_NEWCAMD)
@@ -2797,7 +2797,7 @@ int32_t write_server()
 				if (rdr->cc_version[0] || cfg.http_full_cfg)
 					fprintf_conf(f, "cccversion", "%s\n", rdr->cc_version);
 
-				if (rdr->cc_maxhop != 10 || cfg.http_full_cfg)
+				if (rdr->cc_maxhop != DEFAULT_CC_MAXHOP || cfg.http_full_cfg)
 					fprintf_conf(f, "cccmaxhops", "%d\n", rdr->cc_maxhop);
 
 				if (rdr->cc_mindown > 0 || cfg.http_full_cfg)
@@ -2809,7 +2809,7 @@ int32_t write_server()
 				if (rdr->cc_keepalive || cfg.http_full_cfg)
 					fprintf_conf(f, "ccckeepalive", "%d\n", rdr->cc_keepalive);
 
-				if ((rdr->cc_reshare != cfg.cc_reshare && rdr->cc_reshare != -1) || cfg.http_full_cfg)
+				if (rdr->cc_reshare != DEFAULT_CC_RESHARE || cfg.http_full_cfg)
 					fprintf_conf(f, "cccreshare", "%d\n", rdr->cc_reshare);
 			}
 			else if (rdr->cc_hop > 0 || cfg.http_full_cfg)
@@ -3093,10 +3093,10 @@ struct s_auth *init_userdb()
 			account->monlvl = cfg.mon_level;
 			account->tosleep = cfg.tosleep;
 			account->c35_suppresscmd08 = cfg.c35_suppresscmd08;
-			account->cccmaxhops = 10;
-			account->cccreshare = -1; //-1 = use cfg.
+			account->cccmaxhops = DEFAULT_CC_MAXHOP;
+			account->cccreshare = DEFAULT_CC_RESHARE; // default: use global cfg
 			account->cccignorereshare = -1;
-			account->cccstealth = -1;
+			account->cccstealth = DEFAULT_CC_STEALTH; // default: use global cfg
 			account->ncd_keepalive = cfg.ncd_keepalive;
 			account->firstlogin = 0;
 			for (i = 1; i < CS_MAXCAIDTAB; account->ctab.mask[i++] = 0xffff);
@@ -3823,7 +3823,7 @@ void chk_reader(char *token, char *value, struct s_reader *rdr)
 	}
 
 	if (!strcmp(token, "reconnecttimeout")) {
-		rdr->tcp_rto  = strToIntVal(value, 30);
+		rdr->tcp_rto  = strToIntVal(value, DEFAULT_TCP_RECONNECT_TIMEOUT);
 		return;
 	}
 
@@ -4331,7 +4331,7 @@ void chk_reader(char *token, char *value, struct s_reader *rdr)
 
 	if (!strcmp(token, "cccmaxhop") || !strcmp(token, "cccmaxhops")) { //Schlocke: cccmaxhops is better!
 		// cccam max card distance
-		rdr->cc_maxhop  = strToIntVal(value, 10);
+		rdr->cc_maxhop  = strToIntVal(value, DEFAULT_CC_MAXHOP);
 		return;
 	}
 
@@ -4352,9 +4352,7 @@ void chk_reader(char *token, char *value, struct s_reader *rdr)
 	}
 
 	if (!strcmp(token, "ccchopsaway") || !strcmp(token, "cccreshar")  || !strcmp(token, "cccreshare")) {
-		rdr->cc_reshare = atoi(value);
-		if (rdr->cc_reshare == cfg.cc_reshare)
-			rdr->cc_reshare = -1;
+		rdr->cc_reshare = strToIntVal(value, DEFAULT_CC_RESHARE);
 		return;
 	}
 
@@ -4578,7 +4576,7 @@ int32_t init_readerdb()
 			memset(rdr->hexserial, 0, sizeof(rdr->hexserial));
 			memset(rdr->rom, 0, sizeof(rdr->rom));
 			rdr->enable = 1;
-			rdr->tcp_rto = 30;
+			rdr->tcp_rto = DEFAULT_TCP_RECONNECT_TIMEOUT;
 			rdr->show_cls = 10;
 			rdr->nagra_read = 0;
 			rdr->mhz = 357;
@@ -4586,8 +4584,8 @@ int32_t init_readerdb()
 			rdr->deprecated = 0;
 			rdr->force_irdeto = 0;
 #ifdef MODULE_CCCAM
-			rdr->cc_reshare = -1;
-			rdr->cc_maxhop = 10;
+			rdr->cc_reshare = DEFAULT_CC_RESHARE;
+			rdr->cc_maxhop  = DEFAULT_CC_MAXHOP;
 			rdr->cc_mindown = 0;
 #endif
 #ifdef WITH_LB

module-cccam.c
@@ -1922,7 +1922,7 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l) {
 		card->origin_reader = rdr;
 		card->origin_id = card->id;
 		card->grp = rdr->grp;
-		card->rdr_reshare = rdr->cc_reshare;
+		card->rdr_reshare = rdr->cc_reshare > -1 ? rdr->cc_reshare : cfg.cc_reshare;
 
 		//Check if this card is from us:
 		LL_ITER it = ll_iter_create(card->remote_nodes);
@@ -3123,7 +3123,7 @@ int32_t cc_cli_init_int(struct s_client *cl) {
                 	
 	rdr->tcp_ito = 1; //60sec...This now invokes ph_idle()
 	if (rdr->cc_maxhop < 0)
-		rdr->cc_maxhop = 10;
+		rdr->cc_maxhop = DEFAULT_CC_MAXHOP;
 
 	if (rdr->tcp_rto <= 2)
 		rdr->tcp_rto = 2; // timeout to 120s

module-cccshare.c
@@ -228,15 +228,15 @@ int32_t send_card_to_clients(struct cc_card *card, struct s_client *one_client) 
                 		int32_t is_ext = cc->cccam220 && can_use_ext(card);
                 		int32_t msg = is_ext?MSG_NEW_CARD_SIDINFO:MSG_NEW_CARD;
                         if (card_valid_for_client(cl, card)) {
-								int32_t usr_reshare = cl->account->cccreshare;
+								int8_t usr_reshare = cl->account->cccreshare;
 								if (usr_reshare == -1) usr_reshare = cfg.cc_reshare;
-                                int32_t usr_ignorereshare = cl->account->cccignorereshare;
+                                int8_t usr_ignorereshare = cl->account->cccignorereshare;
                                 if (usr_ignorereshare == -1) usr_ignorereshare = cfg.cc_ignore_reshare;
                                 
-                                int32_t reader_reshare = card->origin_reader?card->rdr_reshare:usr_reshare;
+                                int8_t reader_reshare = card->origin_reader ? card->rdr_reshare : usr_reshare;
                                 if (reader_reshare == -1) reader_reshare = cfg.cc_reshare;
-                                int32_t reshare = (reader_reshare < usr_reshare) ? reader_reshare : usr_reshare;
-								int32_t new_reshare;
+                                int8_t reshare = (reader_reshare < usr_reshare) ? reader_reshare : usr_reshare;
+								int8_t new_reshare;
 								if (card->card_type == CT_CARD_BY_SERVICE_USER)
 									new_reshare = usr_reshare;
 								else if (cfg.cc_ignore_reshare || usr_ignorereshare)
@@ -568,7 +568,7 @@ struct cc_card *create_card2(struct s_reader *rdr, int32_t j, uint16_t caid, uin
     card->origin_reader = rdr;
     if (rdr) {
     	card->grp = rdr->grp;
-    	card->rdr_reshare = rdr->cc_reshare; //copy reshare because reader could go offline
+		card->rdr_reshare = rdr->cc_reshare > -1 ? rdr->cc_reshare : cfg.cc_reshare; //copy reshare because reader could go offline
     	card->sidtabno = rdr->sidtabno;
     	card->hop = rdr->cc_hop;
 	}
@@ -876,8 +876,7 @@ void update_card_list() {
 
             flt = 0;
 
-            int32_t reshare = rdr->cc_reshare;
-            if (reshare == -1) reshare = cfg.cc_reshare;
+            int8_t reshare = rdr->cc_reshare > -1 ? rdr->cc_reshare : cfg.cc_reshare;
             
             //Reader-Services:
             if ((cfg.cc_reshare_services==1||cfg.cc_reshare_services==2||(!rdr->caid && rdr->typ != R_CCCAM && cfg.cc_reshare_services!=4 )) && 

oscam-http.c
@@ -932,6 +932,11 @@ static char *send_oscam_reader_config(struct templatevars *vars, struct uriparam
 			}
 		}
 		reader_ = newrdr->label;
+		newrdr->tcp_rto = DEFAULT_TCP_RECONNECT_TIMEOUT; // default value
+#ifdef MODULE_CCCAM
+		newrdr->cc_maxhop  = DEFAULT_CC_MAXHOP;  // default value
+		newrdr->cc_reshare = DEFAULT_CC_RESHARE; // default value
+#endif
 	} else if(strcmp(getParam(params, "action"), "Save") == 0) {
 
 		rdr = get_reader_by_label(getParam(params, "label"));
@@ -1240,9 +1245,11 @@ static char *send_oscam_reader_config(struct templatevars *vars, struct uriparam
 	tpl_addVar(vars, TPLADD, tpl_getVar(vars, "TMP"), "selected");
 
 #ifdef MODULE_CCCAM
-	tpl_printf(vars, TPLADD, "CCCMAXHOP", "%d", rdr->cc_maxhop);
+	tpl_printf(vars, TPLADD, "CCCMAXHOP",  "%d", rdr->cc_maxhop);
 	tpl_printf(vars, TPLADD, "CCCMINDOWN", "%d", rdr->cc_mindown);
-	tpl_printf(vars, TPLADD, "CCCRESHARE", "%d", (rdr->cc_reshare==-1)?cfg.cc_reshare:rdr->cc_reshare);
+	tpl_printf(vars, TPLADD, "CCCRESHARE", "%d", rdr->cc_reshare);
+	tpl_printf(vars, TPLADD, "RESHARE",    "%d", cfg.cc_reshare);
+
 	if(rdr->cc_want_emu)
 		tpl_addVar(vars, TPLADD, "CCCWANTEMUCHECKED", "checked");
 	if(rdr->cc_keepalive)
@@ -1552,6 +1559,11 @@ static char *send_oscam_user_config_edit(struct templatevars *vars, struct uripa
 		for (i=1; i<CS_MAXCAIDTAB; account->ctab.mask[i++]=0xffff);
 		for (i=1; i<CS_MAXTUNTAB; account->ttab.bt_srvid[i++]=0x0000);
 		account->expirationdate=(time_t)NULL;
+#ifdef MODULE_CCCAM
+		account->cccmaxhops = DEFAULT_CC_MAXHOP;  // default value
+		account->cccreshare = DEFAULT_CC_RESHARE; // default use global conf
+		account->cccstealth = DEFAULT_CC_STEALTH; // default use global conf
+#endif
 #ifdef CS_ANTICASC
 		account->ac_users   = -1; // by default create the new user with global ac_users value
 		account->ac_penalty = -1; // by default create the new user with global penality value
@@ -1755,7 +1767,6 @@ static char *send_oscam_user_config_edit(struct templatevars *vars, struct uripa
 	tpl_addVar(vars, TPLADD, tpl_getVar(vars, "TMP"), "selected");
 
 	tpl_printf(vars, TPLADD, "STEALTH", "%s", cfg.cc_stealth ? "enable" : "disable");
-
 #endif
 
 	//Failban

oscam-http.h
@@ -1272,7 +1272,8 @@ provid=\"##APIPROVIDERPROVID##\">##APIPROVIDERNAME##</provider>\n"
 			</TR>\n\
 			<TR class=\"cfg_cccam\"><TD>##TPLHELPPREFIX##server#cccmaxhops##TPLHELPSUFFIX##Maxhop:</A></TD><TD><input name=\"cccmaxhop\" type=\"text\" size=\"3\" maxlength=\"2\" value=\"##CCCMAXHOP##\"></TD></TR>\n\
 			<TR class=\"cfg_cccam\"><TD>##TPLHELPPREFIX##server#cccmindown##TPLHELPSUFFIX##Mindown:</A></TD><TD><input name=\"cccmindown\" type=\"text\" size=\"3\" maxlength=\"2\" value=\"##CCCMINDOWN##\"></TD></TR>\n\
-			<TR class=\"cfg_cccam\"><TD>##TPLHELPPREFIX##server#cccreshare##TPLHELPSUFFIX##Reshare:</A></TD><TD><input name=\"cccreshare\" type=\"text\" size=\"3\" maxlength=\"2\" value=\"##CCCRESHARE##\"></TD></TR>\n\
+			<TR class=\"cfg_cccam\"><TD>##TPLHELPPREFIX##server#cccreshare##TPLHELPSUFFIX##Reshare:</A></TD><TD><input name=\"cccreshare\" type=\"text\" size=\"3\" maxlength=\"2\" value=\"##CCCRESHARE##\">\
+				&nbsp;Global CCCam Reshare:<SPAN CLASS=\"global_conf\" TITLE=\"This value is used if Reshare = -1\"><A HREF=\"config.html?part=cccam\">##RESHARE##</A></SPAN></TD></TR>\n\
 			<TR class=\"cfg_cccam\"><TD>##TPLHELPPREFIX##server#cccwantemu##TPLHELPSUFFIX##Want Emu:</A><input name=\"cccwantemu\" type=\"hidden\" value=\"0\"></TD><TD><input name=\"cccwantemu\" type=\"checkbox\" value=\"1\" ##CCCWANTEMUCHECKED##></TD></TR>\n\
 			<TR class=\"cfg_cccam\"><TD>##TPLHELPPREFIX##server#reconnecttimeout##TPLHELPSUFFIX##Reconnect-timeout:</A></TD><TD><input name=\"reconnecttimeout\" type=\"text\" size=\"5\" maxlength=\"5\" value=\"##RECEIVETIMEOUT##\"></TD></TR>\n\
 			<TR class=\"cfg_cccam\"><TD>##TPLHELPPREFIX##server#ccckeepalive##TPLHELPSUFFIX##Keep alive:</A></TD><TD><SELECT NAME=\"ccckeepalive\"><OPTION VALUE=\"0\">NO</OPTION><OPTION VALUE=\"1\" ##KEEPALIVECHECKED##>YES</OPTION></SELECT></TD></TR>\n"

commit 0da31e691221060c3d9b614ad87fd8e0e57f62a8

Fix bug #2063 introduced in r5762
git-svn-id: https://svn.streamboard.tv/oscam/trunk@5768 4b0bc96b-bc66-0410-9d44-ebda105a78c1

globals.h
@@ -396,13 +396,18 @@ extern void cs_switch_led(int32_t led, int32_t action);
  * =========================== */
 #define DEFAULT_TCP_RECONNECT_TIMEOUT 30
 #define DEFAULT_NCD_KEEPALIVE 1
+
+#ifdef MODULE_CCCAM
 #define DEFAULT_CC_MAXHOP   10
 #define DEFAULT_CC_RESHARE  -1 // Use global cfg
 #define DEFAULT_CC_STEALTH  -1 // Use global cfg
 #define DEFAULT_CC_KEEPALIVE 0
+#endif
+
+#ifdef CS_ANTICASC
 #define DEFAULT_AC_USERS   -1 // Use global cfg
 #define DEFAULT_AC_PENALTY -1 // Use global cfg
-
+#endif
 
 /* ===========================
  *      global structures
@@ -1109,10 +1114,12 @@ struct s_auth
 	int8_t			c35_suppresscmd08;
 	uint8_t			c35_sleepsend;
 	int8_t			ncd_keepalive;
+#ifdef MODULE_CCCAM
 	int32_t			cccmaxhops;
 	int8_t			cccreshare;
 	int8_t			cccignorereshare;
 	int8_t			cccstealth;
+#endif
 	int8_t			disabled;
 	int32_t			failban;
 

oscam-config.c
@@ -3093,20 +3093,22 @@ struct s_auth *init_userdb()
 			account->monlvl = cfg.mon_level;
 			account->tosleep = cfg.tosleep;
 			account->c35_suppresscmd08 = cfg.c35_suppresscmd08;
+			account->ncd_keepalive = cfg.ncd_keepalive;
+			account->firstlogin = 0;
+#ifdef MODULE_CCCAM
 			account->cccmaxhops = DEFAULT_CC_MAXHOP;
 			account->cccreshare = DEFAULT_CC_RESHARE; // default: use global cfg
 			account->cccignorereshare = -1;
 			account->cccstealth = DEFAULT_CC_STEALTH; // default: use global cfg
-			account->ncd_keepalive = cfg.ncd_keepalive;
-			account->firstlogin = 0;
-			for (i = 1; i < CS_MAXCAIDTAB; account->ctab.mask[i++] = 0xffff);
-			for (i = 1; i < CS_MAXTUNTAB; account->ttab.bt_srvid[i++] = 0x0000);
-			nr++;
-
+#endif
 #ifdef CS_ANTICASC
 			account->ac_users   = DEFAULT_AC_USERS;   // use ac_users global value
 			account->ac_penalty = DEFAULT_AC_PENALTY; // use ac_penalty global value
 #endif
+			for (i = 1; i < CS_MAXCAIDTAB; account->ctab.mask[i++] = 0xffff);
+			for (i = 1; i < CS_MAXTUNTAB; account->ttab.bt_srvid[i++] = 0x0000);
+			nr++;
+
 			continue;
 		}
 